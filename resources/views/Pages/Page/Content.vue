3eacdf9540e89ba9769794503e8d9bbcc7fe1a4d
jonpz <jon.pza@gmail.com>
2024-03-17 20:25:25 -0400

    what the? this works now

--- resources/views/Pages/Page/Content.vue
+++ resources/views/Pages/Page/Content.vue
@@ -24,7 +24,7 @@ const BlockCommonTitle = defineAsyncComponent(
     <div v-if="item?.blocks && Array.isArray(item.blocks) && item.blocks.length > 0
         " class="mx-auto w-full max-w-6xl">
         <div v-for="(block, index) in item.blocks" :key="index">
-            <BlockCommonTitle v-if="block.type == 'app-title'" :block="block"></BlockCommonTitle>
+            <BlockCommonTitle v-if="block.type == 'common-title'" :block="block"></BlockCommonTitle>
         </div>
     </div>
 </template>
