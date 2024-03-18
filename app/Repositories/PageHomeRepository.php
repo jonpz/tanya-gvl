ace43ff54b532caaf65985534e32688d3bd64336
jonpz <jon.pza@gmail.com>
2024-03-17 21:58:51 -0400

    clear cache on homepage update

--- app/Repositories/PageHomeRepository.php
+++ app/Repositories/PageHomeRepository.php
@@ -3,18 +3,29 @@
 namespace App\Repositories;
 
 use A17\Twill\Repositories\Behaviors\HandleBlocks;
-use A17\Twill\Repositories\Behaviors\HandleTranslations;
 use A17\Twill\Repositories\Behaviors\HandleMedias;
 use A17\Twill\Repositories\Behaviors\HandleRevisions;
+use A17\Twill\Repositories\Behaviors\HandleTranslations;
 use A17\Twill\Repositories\ModuleRepository;
 use App\Models\PageHome;
+use Illuminate\Support\Facades\Cache;
 
 class PageHomeRepository extends ModuleRepository
 {
-    use HandleBlocks, HandleTranslations, HandleMedias, HandleRevisions;
+    use HandleBlocks, HandleMedias, HandleRevisions, HandleTranslations;
 
     public function __construct(PageHome $model)
     {
         $this->model = $model;
+    }
+
+    public function afterSave($object, $fields): void
+    {
+        // Cache clearing.
+        foreach (optional($object)->translations as $translation) {
+            Cache::forget('page.home.'.$translation->locale);
+        }
+
+        parent::afterSave($object, $fields);
     }
 }
